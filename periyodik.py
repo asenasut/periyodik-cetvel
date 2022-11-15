c3ae21e2e44489e47d1d96cf4303f3c68f7e37da
asena <115923266+asenasut@users.noreply.github.com>
2022-11-16 00:47:54 +0300

    Add files via upload

---
+++ periyodik.py
@@ -0,0 +1,15 @@
+element= input("2A grubundaki elementlerden birisini yazınız:")
+if element == 'berilyum':
+    print("Sembolü: Be, Numarası:4")
+elif element == 'magnezyum':
+    print("Sembolü: Mg, Numarası:12")
+elif element == 'kalsiyum':
+    print("Sembolü:Ca, Numarası:20")
+elif element == 'stronsiyum':
+    print("Sembolü:Sr, Numarası:38")
+elif element == 'baryum':
+    print("Sembolü: Ba, Numarası:56")
+elif element == 'radyum':
+    print("Sembolü: Ra, Numarası:88")
+else:
+    print("Girdiğiniz element 2A grubunda bulunmamaktadır.")
